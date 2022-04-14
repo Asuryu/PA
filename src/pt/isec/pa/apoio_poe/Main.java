f308b6a05c368ef2ff26f5c68535f6140c1d76dd
Asuryu <tomas-250@outlook.com>
2022-04-14 16:31:08 +0100

    Update Main.java

--- src/pt/isec/pa/apoio_poe/Main.java
+++ src/pt/isec/pa/apoio_poe/Main.java
@@ -11,14 +11,6 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         PoEContext fsm = new PoEContext();
         PoEUI ui = new PoEUI(fsm);
-
-        Scanner sc = new Scanner(new File("src/pt/isec/pa/apoio_poe/csv/alunos.csv"));
-        sc.useDelimiter(",");
-        while(sc.hasNext()){
-            System.out.print(sc.next() + "  ");
-        }
-        sc.close();
-        System.out.println("\n");
         ui.start();
     }
 }
