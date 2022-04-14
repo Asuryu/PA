6936a065ced1d4ff6b58c2759a2c9ebe64cdde58
Asuryu <tomas-250@outlook.com>
2022-04-14 01:21:21 +0100

    Abrir e ler ficheiros CSV

--- src/pt/isec/pa/apoio_poe/Main.java
+++ src/pt/isec/pa/apoio_poe/Main.java
@@ -1,15 +1,24 @@
 package pt.isec.pa.apoio_poe;
 
-import pt.isec.pa.apoio_poe.model.data.PoEData;
 import pt.isec.pa.apoio_poe.model.fsm.PoEContext;
 import pt.isec.pa.apoio_poe.ui.text.PoEUI;
-import pt.isec.pa.apoio_poe.utils.PAInput;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        PAInput.chooseOption("Escolha uma opção", "Gestão de Alunos", "Gestão de Docentes", "Gestão de propostas de estágio ou projeto", "Sair");
+    public static void main(String[] args) throws FileNotFoundException {
         PoEContext fsm = new PoEContext();
         PoEUI ui = new PoEUI(fsm);
+
+        Scanner sc = new Scanner(new File("src/pt/isec/pa/apoio_poe/csv/alunos.csv"));
+        sc.useDelimiter(",");
+        while(sc.hasNext()){
+            System.out.print(sc.next() + "  ");
+        }
+        sc.close();
+        System.out.println("\n");
         ui.start();
     }
 }
