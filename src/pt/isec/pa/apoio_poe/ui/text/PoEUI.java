6c213782bf522b7f27788d2c8ecdbdf2ddf64bda
Asuryu <tomas-250@outlook.com>
2022-04-14 01:22:46 +0100

    Experimentação com a UI

--- src/pt/isec/pa/apoio_poe/ui/text/PoEUI.java
+++ src/pt/isec/pa/apoio_poe/ui/text/PoEUI.java
@@ -1,36 +1,48 @@
 package pt.isec.pa.apoio_poe.ui.text;
 
 import pt.isec.pa.apoio_poe.model.fsm.PoEContext;
+import pt.isec.pa.apoio_poe.utils.PAInput;
 
 public class PoEUI {
     private PoEContext fsm;
+    private boolean exit = false;
 
     public PoEUI(PoEContext fsm) {
         this.fsm = fsm;
     }
 
     public void start() {
-        if(fsm.getState() == null) System.exit(-1);
-        switch(fsm.getState()){
-            case CONFIG:
-                System.out.println("Por implementar CONFIG!\n");
-                fsm.nextPhase();
-                System.out.println(fsm.getState());
-                fsm.previousPhase();
-                System.out.println(fsm.getState());
-                break;
-            case APPLICATION_OPT:
-                System.out.println("Por implementar APPLICATION_OPT!\n");
-                break;
-            case PROP_ATTRIBUTION:
-                System.out.println("Por implementar PROP_ATTRIBUTION!\n");
-                break;
-            case ORI_ATTRIBUTION:
-                System.out.println("Por implementar ORI_ATTRIBUTION!\n");
-                break;
-            case REVIEW:
-                System.out.println("Por implementar REVIEW!\n");
-                break;
+        while(!exit){
+            System.out.println("STATE: " + fsm.getState());
+            String comando = PAInput.readString("> ", false);
+            switch(fsm.getState()){
+                case CONFIG:
+                    System.out.println("Por implementar CONFIG!\n");
+                    //int option = PAInput.chooseOption("Escolha uma opção", "Gestão de Alunos", "Gestão de Docentes", "Gestão de propostas de estágio ou projeto", "Sair");
+                    if(comando.equalsIgnoreCase("next")) {
+                        fsm.nextPhase();
+                    }
+                    else if(comando.equalsIgnoreCase("exit")) exit = true;
+                    break;
+                case APPLICATION_OPT:
+                    System.out.println("Por implementar APPLICATION_OPT!\n");
+                    if(comando.equalsIgnoreCase("next")) fsm.nextPhase();
+                    else if(comando.equalsIgnoreCase("previous")) fsm.previousPhase();
+                    else if(comando.equalsIgnoreCase("exit")) exit = true;
+                    break;
+                case PROP_ATTRIBUTION:
+                    System.out.println("Por implementar PROP_ATTRIBUTION!\n");
+                    break;
+                case ORI_ATTRIBUTION:
+                    System.out.println("Por implementar ORI_ATTRIBUTION!\n");
+                    break;
+                case REVIEW:
+                    System.out.println("Por implementar REVIEW!\n");
+                    break;
+                default:
+                    System.exit(-1);
+                    break;
+            }
         }
     }
 }
