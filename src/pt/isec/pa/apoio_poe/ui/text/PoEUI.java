25bb514990ead2076611f1e60988ea7d64a12b34
Asuryu <tomas-250@outlook.com>
2022-04-15 17:23:57 +0100

    Cleanup do UI

--- src/pt/isec/pa/apoio_poe/ui/text/PoEUI.java
+++ src/pt/isec/pa/apoio_poe/ui/text/PoEUI.java
@@ -1,14 +1,12 @@
 package pt.isec.pa.apoio_poe.ui.text;
 
-import com.sun.tools.jconsole.JConsoleContext;
 import pt.isec.pa.apoio_poe.model.fsm.PoEContext;
-import pt.isec.pa.apoio_poe.model.fsm.PoEState;
+import pt.isec.pa.apoio_poe.utils.Menu;
 import pt.isec.pa.apoio_poe.utils.PAInput;
-
-import java.io.FileNotFoundException;
+import pt.isec.pa.apoio_poe.utils.Utils;
 
 public class PoEUI {
-    private PoEContext fsm;
+    private final PoEContext fsm;
     private boolean exit = false;
 
     public PoEUI(PoEContext fsm) {
@@ -17,37 +15,37 @@ public class PoEUI {
 
     public void start() {
 
-        System.out.println(mostrarASCII());
-        boolean firstTime = true;
+        Utils.mostrarASCII();
+        boolean changeMode = true;
         int option = 0;
 
         while(!exit){
-            System.out.println("STATE: " + fsm.getState());
-            System.out.println("CLOSED: " + fsm.isClosed());
-            if(firstTime){
-                option = PAInput.chooseOption("Escolha uma opção", "Gestão de Alunos", "Gestão de Docentes", "Gestão de propostas de estágio ou projeto");
-                firstTime = false;
+            if(changeMode){
+                option = PAInput.chooseOption("Escolha uma opção", "Gestão de Alunos", "Gestão de Docentes", "Gestão de propostas de estágio ou projeto", "Próxima fase", "Fechar a fase", "Sair");
+                changeMode = false;
+            } else {
+                System.out.println("STATE: " + fsm.getState());
+                System.out.println("CLOSED: " + fsm.isClosed());
             }
-            String comando = PAInput.readString("> ", false);
             switch(fsm.getState()){
                 case CONFIG:
-                    switch (comando){
-                        case "next" -> fsm.nextPhase();
-                        case "close" -> fsm.closePhase();
-                        case "add" -> {
-                            if(option == 1) fsm.addAlunosCSV();
-                            else if(option == 2) fsm.addDocentesCSV();
-                            else if(option == 3) fsm.addPropostasCSV();
-                            else if(option == 4) exit = true;
+                    switch (option){
+                        case 1 -> {
+                            changeMode = Menu.menuAlunos(fsm);
                         }
-                        case "mode" -> option = PAInput.chooseOption("Escolha uma opção", "Gestão de Alunos", "Gestão de Docentes", "Gestão de propostas de estágio ou projeto", "Sair");
-                        case "exit" -> exit = true;
+                        case 2 -> {
+                            changeMode = Menu.menuDocentes(fsm);
+                        }
+                        case 3 -> {
+                            changeMode = Menu.menuPropostas(fsm);
+                        }
+                        case 4 -> fsm.nextPhase();
+                        case 5 -> fsm.closePhase();
+                        case 6 -> System.exit(0);
                     }
                     break;
                 case APPLICATION_OPT:
-                    if(comando.equalsIgnoreCase("next")) fsm.nextPhase();
-                    else if(comando.equalsIgnoreCase("previous")) fsm.previousPhase();
-                    else if(comando.equalsIgnoreCase("exit")) exit = true;
+                    System.out.println("Por implementar APPLICATION_OPT!\n");
                     break;
                 case PROP_ATTRIBUTION:
                     System.out.println("Por implementar PROP_ATTRIBUTION!\n");
@@ -63,14 +61,6 @@ public class PoEUI {
                     break;
             }
         }
-    }
-
-    public String mostrarASCII(){
-        return( "  ____       _____   ____  _____ ___ ____  \n" +
-                " |  _ \\ ___ | ____| |  _ \\| ____|_ _/ ___|\n" +
-                " | |_) / _ \\|  _|   | | | |  _|  | |\\___ \\ \n" +
-                " |  __/ (_) | |___  | |_| | |___ | | ___) |\n" +
-                " |_|   \\___/|_____| |____/|_____|___|____/ \n");
     }
 
 }
